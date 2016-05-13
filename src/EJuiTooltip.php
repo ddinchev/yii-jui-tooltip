cabfc346724c7a3ebdf949d0dab9de1561d2fb6c
Dimitar Dinchev <dimitar.dinchev@skyscanner.jp>
2016-05-14 01:04:16 +0900

    Several fixes.

--- src/EJuiTooltip.php
+++ src/EJuiTooltip.php
@@ -3,21 +3,21 @@
 Yii::import('zii.widgets.jui.CJuiWidget');
 
 /**
- * EJuiTooltip class file.
+ * EJuiTooltio class file.
  *
- * EJuiTooltip displays a tooltip widget.
+ * EJuiTooltio displays a tooltip widget.
  *
- * EJuiTooltip encapsulates the {@link http://jqueryui.com/demos/tooltip/ JUI Tooltip}
+ * EJuiTooltio encapsulates the {@link http://jqueryui.com/demos/tooltip/ JUI Tooltip}
  * plugin.
  *
  * To use this widget, you may insert the following code in a view:
  * <pre>
- * $this->widget('application.extensions.juitooltip.EJuiTooltip',array(
+ * $this->widget('application.extensions.juitooltip.EJuiTooltio', array(
  *     // elements selector, by default their title attribute will be used as content
- *     'selector'=>'.tooltip',
+ *     'selector' => '.tooltip',
  *     // additional javascript options for the tooltip plugin
- *     'options'=>array(
- *         'scope'=>'myScope',
+ *     'options' => array(
+ *         'scope' => 'myScope',
  *     ),
  * ));
  *
@@ -25,27 +25,53 @@ Yii::import('zii.widgets.jui.CJuiWidget');
  *
  * By configuring the {@link options} property, you may specify the options
  * that need to be passed to the JUI Tooltip plugin. Please refer to
- * the {@link http://jqueryui.com/demos/tooltip/ JUI Tooltip} documentation
+ * the {@link http://jqueryui.com/demos/tooltip/ JUI Draggable} documentation
  * for possible options (name-value pairs).
  *
  *
  * @author Dimitar Dinchev <dinchev.dimitar@gmail.com>
  * @link http://www.yiiframework.com/
-*/
+ */
 class EJuiTooltip extends CJuiWidget
 {
-	/**
-	 * @var tooltip selector, defaults to document (all elements with title attribute
-	 * will have tooltip with the title attribute content used as tooltip content)
-	 */
-	public $selector = 'document';
+    /**
+     * @var string Tooltip selector, defaults to document (all elements with title attribute
+     * will have tooltip with the title attribute content used as tooltip content)
+     */
+    public $selector = 'document';
 
-	/**
-	 * Renders the close tag of the tooltip element.
-	 */
-	public function run()
-	{
-		$options = CJavaScript::encode($this->options);
-		Yii::app()->getClientScript()->registerScript(__CLASS__ . $this->selector, "jQuery('{$this->selector}').tooltip($options);");
-	}
+    /**
+     * Renders the close tag of the draggable element.
+     */
+    public function run()
+    {
+        Yii::app()->clientScript->registerScript(__CLASS__ . $this->selector,
+            $this->getJavascriptString($this->selector, $this->options)
+        );
+    }
+
+    /**
+     * @param $selector
+     * @param array $options
+     * @return string
+     */
+    public function getJavascriptString($selector, array $options = [])
+    {
+        if (!isset($options['content'])) {
+            $options['content'] = 'js: function() { return $(this).prop("title"); }';
+        }
+        $options = CJavaScript::encode($options);
+        return "$('{$selector}').tooltip($options)";
+    }
+
+    /**
+     * Helper method for afterAjaxUpdate calls
+     * @param $selector
+     * @param array $options
+     * @return CJavaScriptExpression
+     */
+    public function getJavascriptFunction($selector, array $options = [])
+    {
+        return new CJavaScriptExpression(sprintf('function () { %s }', $this->getJavascriptString($selector, $options)));
+    }
 }
